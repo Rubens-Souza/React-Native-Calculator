051a3360a551c0fb1293da5d1500f02b55bda0da
Rubens Souza <rubens.souza@dtidigital.com.br>
2020-10-14 14:36:58 -0300

    init project

---
+++ Calculator/src/App/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+const App = () => {
+
+    return (
+        <SafeAreaView>
+            <Text>Hi!!!</Text>
+        </SafeAreaView>
+    );
+
+};
+
+export default App;
