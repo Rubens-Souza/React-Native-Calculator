c3063792d44d47650bf1f0fd563c48b37ebcba5b
Rubens Souza <rubens.souza@dtidigital.com.br>
2020-10-15 16:31:57 -0300

    feat(constants): Create a Operations constant

---
+++ Calculator/src/shared/constants/Operations.js
@@ -0,0 +1,12 @@
+const Operations = {
+    Clear: "CE",
+    Negative: "+/-",
+    Porcentaje: '%',
+    Division: '/',
+    Multiplication: '*',
+    Subtraction: '-',
+    Sum: '+',
+    Equals: '=',
+};
+
+export default Operations;
