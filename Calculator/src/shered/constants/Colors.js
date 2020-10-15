b406df0c7725890195ec79cbc7dee68d60d24eac
Rubens Souza <rubens.souza@dtidigital.com.br>
2020-10-15 12:41:58 -0300

    feature(colors): Add more colors to the palette

--- Calculator/src/shered/constants/Colors.js
+++ Calculator/src/shered/constants/Colors.js
@@ -1,6 +1,8 @@
 const Colors = {
-    LightBlue: "#52C9DC",
-    White: "#FFFFFF",
+    LightBlue: "#52c9dc",
+    BluishLightGrey: "#f2f8fc",
+    White: "#ffffff",
+    SoftBlack: "#454545",
 };
 
 export default Colors;
