6deeb7a61d61e150c72d0dc396aaeaf4ed3ca192
Rubens Souza <rubens.souza@dtidigital.com.br>
2020-10-15 14:48:23 -0300

    fix(style): Fix Result font size

--- Calculator/src/features/Display/Result/Styles.js
+++ Calculator/src/features/Display/Result/Styles.js
@@ -4,21 +4,21 @@ import styled from "styled-components/native";
 export const StyledResultText = styled.Text`
     color: ${Colors.SoftBlack};
     font-weight: bold;
-    font-size: 30;
+    font-size: 50px;
 `;
 
 export const StyledEqualsSign = styled.Text`
     color: ${Colors.LightBlue};
     font-weight: bold;
-    font-size: 30;
+    font-size: 50px;
 `;
 
 export const StyledResultGroup = styled.View`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-    border-bottom-width: 1px;
-    border-bottom-color: ${Colors.SoftBlack};
-    padding-bottom: 6px;
-    margin-bottom: 6px;
+    border-bottom-width: 2px;
+    border-bottom-color: ${Colors.LightGray};
+    padding-bottom: 12px;
+    margin-bottom: 21px;
 `;
