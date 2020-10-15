f42711794f94e131377d2dfaf45ead1dae8cab13
Rubens Souza <rubens.souza@dtidigital.com.br>
2020-10-15 15:36:29 -0300

    fix(feature): Center calculator UI

--- Calculator/src/features/Calculator/Styles.js
+++ Calculator/src/features/Calculator/Styles.js
@@ -1,7 +1,10 @@
+import Colors from "../../shared/constants/Colors";
 import styled from "styled-components/native";
 
 export const StyledSafeAreaView = styled.SafeAreaView`
     flex: 1;
     display: flex;
+    justify-content: center;
     padding: 25px 25px;
+    background-color: ${Colors.White};
 `;
