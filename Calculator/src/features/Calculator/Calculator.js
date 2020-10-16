c47f91ac791383d4d56a48c5c55e974a1e3910a3
Rubens Souza <rubens.souza@dtidigital.com.br>
2020-10-16 11:38:05 -0300

    feat(features): Create Calculator hanlders for numbers

--- Calculator/src/features/Calculator/Calculator.js
+++ Calculator/src/features/Calculator/Calculator.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Display from "../Display/Display";
 import Keyboard from "../Keyboard/Keyboard";
 
 import { StyledSafeAreaView } from "./Styles";
 
+import StringUtils from "../../shared/utils/StringUtils";
+
 const Calculator = () => {
+
+    const [actualOperation, setActualOperation] = useState("");
+    const [numbers, setNumbers] = useState(new Array(2));
+    const [expression, setExpression] = useState("");
+
+    var actualNumberIndex = 0;
+
+    const handleNumberPress = (number) => {
+        numbers[actualNumberIndex] = number;
+        setExpression(`${StringUtils.getDefaultIfEmpty(numbers[0])} ${StringUtils.getDefaultIfBlank(actualOperation)} ${StringUtils.getDefaultIfEmpty(numbers[1])}`);
+    };
+
+    const handleOperationPress = (operation, lastNumber) => {
+
+    };
 
     return (
         <StyledSafeAreaView>
-            <Display/>
-            <Keyboard/>
+            <Display formula={expression}/>
+            <Keyboard onNumberPress={handleNumberPress}/>
         </StyledSafeAreaView>
     );
 
