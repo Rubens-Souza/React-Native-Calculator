eeb7ea59386308c01c3686a720bf4eedbff1b477
Rubens Souza <rubens.souza@dtidigital.com.br>
2020-10-19 10:00:34 -0300

    feat(utils): Create formatting functions

--- Calculator/src/shared/utils/Utils.js
+++ Calculator/src/shared/utils/Utils.js
@@ -1,3 +1,7 @@
-export const hasSetFunctionProperty = (value) => {
-    return typeof(value) === "function";
-};
+import Operations from "../constants/Operations";
+
+export const hasSetFunctionProperty = (value) => typeof(value) === "function";
+
+export const formatDivisionSigns = (expression) => expression.replace(Operations.Division.interfaceSign, Operations.Division.operationSign);
+
+export const formatMultiplicationSigns = (expression) => expression.replace(Operations.Multiplication.interfaceSign, Operations.Multiplication.operationSign);
